bbdc42c3d8e00aaf82a1daf7f35cd0429fd1c5a5
Niels Nielsen <niels.nielsen@gmail.com>
2025-12-08 16:14:59 -0700

    evaluate initial JS files in order

---
+++ src/ui/js/components/statusRing.js
@@ -0,0 +1,8 @@
+console.log("status ring")
+
+const statusRingNode = document.getElementById("status_ring_group")
+statusRingNode.innerHTML = ""
+
+statusRingNode.innerHTML = `
+<circle cx="0" cy="0" r="0.5" stroke="red" />
+`
--- src/ui/js/setup.js
+++ src/ui/js/setup.js
@@ -2,16 +2,37 @@
 const CSS_FILE = "css/main.css"
 // const cssEndsWith = FILE_PREFIX + CSS_FILE
 
+const jsFiles = ["js/components/statusRing.js"]
+
 const cssElement = document.getElementById("main_style")
 
 const handleCss = (contents) => {
   cssElement.innerText = contents
 }
 
+const handleJs = (contents) => {
+  try {
+    eval(contents)
+  } catch (e) {
+    window._lastEvalError = e
+    console.log(e)
+  }
+}
+
 const initFiles = async () => {
   await fetch(CSS_FILE)
     .then((r) => r.text())
     .then(handleCss)
+  const jsFetches = jsFiles.map(async (fileName) => {
+    return fetch(fileName).then((r) => r.text())
+  })
+  const jsResults = await Promise.all(jsFetches)
+  requestAnimationFrame(() => {
+    // wait for one frame to make sure css is applied first
+    jsResults.forEach((contents) => {
+      handleJs(contents)
+    })
+  })
 }
 
 await initFiles()
