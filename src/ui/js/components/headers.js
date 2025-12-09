93f310c4df37138bcb3104f3f1676aec4bdbded7
Niels Nielsen <niels.nielsen@gmail.com>
2025-12-09 16:13:44 -0700

    cleanup

--- src/ui/js/components/headers.js
+++ src/ui/js/components/headers.js
@@ -3,12 +3,12 @@ const { drawText } = __WTR__
 const parentGroup = document.getElementById("headers_group")
 parentGroup.innerHTML = ""
 
-drawText({ x: -0.86, y: -0.73, text: "editors", parentNode: parentGroup })
+drawText({ x: -0.86, y: -0.73, text: "editors", className: "header", parentNode: parentGroup })
 
 drawText({
   x: 0.86,
   y: -0.73,
   text: "clients",
-  className: "right_header",
+  className: ["header", "right_header"],
   parentNode: parentGroup,
 })
